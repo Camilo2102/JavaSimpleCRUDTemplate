e450308a8dcf0d141cd552f98c11e11a2d7b0d7e
CamiloMD21 <117365650+CamiloMD21@users.noreply.github.com>
2024-10-28 17:21:19 -0500

    Remove mongo default exclude

--- src/main/java/cloud/webgen/web/crud/template/webgensimplecrudtemplate/infrastructure/inbound/config/ProjectConfig.java
+++ src/main/java/cloud/webgen/web/crud/template/webgensimplecrudtemplate/infrastructure/inbound/config/ProjectConfig.java
@@ -1,12 +1,9 @@
 package cloud.webgen.web.crud.template.webgensimplecrudtemplate.infrastructure.inbound.config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("cloud.webgen.web.crud.mongodb")
-@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class ProjectConfig{
 }
