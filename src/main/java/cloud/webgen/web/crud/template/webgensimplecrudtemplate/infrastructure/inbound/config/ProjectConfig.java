14106c479014e1f27865830277314ad5f08b3d51
CamiloMD21 <117365650+CamiloMD21@users.noreply.github.com>
2024-10-28 16:38:18 -0500

    Exclusion de configuracion automatica de mongo

--- src/main/java/cloud/webgen/web/crud/template/webgensimplecrudtemplate/infrastructure/inbound/config/ProjectConfig.java
+++ src/main/java/cloud/webgen/web/crud/template/webgensimplecrudtemplate/infrastructure/inbound/config/ProjectConfig.java
@@ -1,12 +1,12 @@
 package cloud.webgen.web.crud.template.webgensimplecrudtemplate.infrastructure.inbound.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan("cloud.webgen.web.crud.mongodb")
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class ProjectConfig{
 }
