20f894427e1118171c8a57f9eac2d26066bd247b
Camilo2102 <82391219+Camilo2102@users.noreply.github.com>
2025-01-15 01:04:59 -0500

    Create OptionController.java

---
+++ src/main/java/cloud/webgen/web/crud/template/webgensimplecrudtemplate/infrastructure/inbound/controllers/OptionController.java
@@ -0,0 +1,19 @@
+package cloud.webgen.web.crud.template.webgensimplecrudtemplate.infrastructure.inbound.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class OptionController {
+
+    @RequestMapping(value = "/**", method = RequestMethod.OPTIONS)
+    public ResponseEntity<Void> handleOptions() {
+        return ResponseEntity.ok()
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+                .header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+                .build();
+    }
+}
